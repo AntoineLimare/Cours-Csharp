b274d8c1918d8122cabfb174fa8212013c3f8f07
antoine <antoinelimare76700@gmail.com>
2025-03-24 14:19:36 +0100

    Etape 3 TP1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            // Récupération du salaire annuel
+            Console.Write("Entrez votre salaire annuel (en euros) : ");
+            double salaireAnnuel = Convert.ToDouble(Console.ReadLine());
+
+            // Récupération du taux d'imposition
+            Console.Write("Entrez votre taux d'imposition (en pourcentage) : ");
+            double tauxImposition = Convert.ToDouble(Console.ReadLine());
+
+            // Calcul du salaire annuel net
+            double salaireAnnuelNet = salaireAnnuel * (1 - (tauxImposition / 100));
+
+            // Calcul du salaire mensuel net
+            double salaireMensuelNet = salaireAnnuelNet / 12;
+
+            // Affichage du résultat
+            Console.WriteLine($"Votre salaire net mensuel est de : {salaireMensuelNet:F2} euros");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Erreur : Veuillez entrer des valeurs numériques valides.");
+        }
+    }
+}
