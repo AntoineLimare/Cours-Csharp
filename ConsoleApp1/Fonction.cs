7653fa5ea1e6cd36b069d37709e91b4430dc4b73
antoine <antoinelimare76700@gmail.com>
2025-03-25 10:16:15 +0100

    Choix 1 ou 2

---
+++ ConsoleApp1/Fonction.cs
@@ -0,0 +1,69 @@
+﻿
+
+class Fonction
+{
+    public static void SalaireMensuelNet()
+    {
+        Console.Write("Entrez votre salaire annuel (en euros) : ");
+        double salaireAnnuel = Convert.ToDouble(Console.ReadLine());
+
+        // Récupération du taux d'imposition
+        Console.Write("Entrez votre taux d'imposition (en pourcentage) : ");
+        double tauxImposition = Convert.ToDouble(Console.ReadLine());
+        
+        double salaireAnnuelNet = salaireAnnuel * (1 - (tauxImposition / 100));
+
+        // Calcul du salaire mensuel brut et net
+        double salaireMensuelBrut = salaireAnnuel / 12;
+        double salaireMensuelNet = salaireAnnuelNet / 12;
+        Console.WriteLine($"Votre salaire net mensuel est de : {salaireMensuelNet:F2} euros");
+
+    }// Calcul du salaire annuel net
+    
+    public static void TableauSalaire()
+    {
+        // Récupération du taux d'imposition
+        Console.Write("Entrez votre prime de noël (en pourcentage) : ");
+        double primeNoel = Convert.ToDouble(Console.ReadLine());
+        
+        
+        // Déclaration et initialisation des variables
+        string[] mois = {
+            "Janvier", "Février", "Mars", "Avril", "Mai", "Juin",
+            "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"
+        };
+
+
+        // Affichage du tableau structuré
+        Console.WriteLine("+------------+---------+");
+        Console.WriteLine("| Mois       | Salaire |");
+        Console.WriteLine("+------------+---------+");
+
+        foreach (string m in mois)
+        {
+            if (m == "Décembre")
+            {
+
+                Console.WriteLine($"| {m,-10} | {salaireMensuelNet * (1 + primeNoel / 100):F2} |");
+            }
+
+            else if (m == "Août")
+            {
+                Console.WriteLine($"| {m,-10} | {0:F2} |");
+            }
+
+            else
+            {
+                // Salaire fixe pour tous les mois
+                Console.WriteLine($"| {m,-10} | {salaireMensuelNet:F2} |");
+            }
+        }
+
+        Console.WriteLine("+------------+---------+");
+    }
+
+}
+
+
+
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,61 +6,18 @@ class Program
     {
         try
         {
-            // Récupération du salaire annuel
-            Console.Write("Entrez votre salaire annuel (en euros) : ");
-            double salaireAnnuel = Convert.ToDouble(Console.ReadLine());
-
-            // Récupération du taux d'imposition
-            Console.Write("Entrez votre taux d'imposition (en pourcentage) : ");
-            double tauxImposition = Convert.ToDouble(Console.ReadLine());
-
-            // Récupération du taux d'imposition
-            Console.Write("Entrez votre prime de noël (en pourcentage) : ");
-            double primeNoel = Convert.ToDouble(Console.ReadLine());
-
-            // Calcul du salaire annuel net
-            double salaireAnnuelNet = salaireAnnuel * (1 - (tauxImposition / 100));
-
-            // Calcul du salaire mensuel brut et net
-            double salaireMensuelBrut = salaireAnnuel / 12;
-            double salaireMensuelNet = salaireAnnuelNet / 12;
-
-            // Affichage du résultat
-            Console.WriteLine($"Votre salaire net mensuel est de : {salaireMensuelNet:F2} euros");
-
-            // Déclaration et initialisation des variables
-            string[] mois = {
-            "Janvier", "Février", "Mars", "Avril", "Mai", "Juin",
-            "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"
-        };
-            
-
-            // Affichage du tableau structuré
-            Console.WriteLine("+------------+---------+");
-            Console.WriteLine("| Mois       | Salaire |");
-            Console.WriteLine("+------------+---------+");
-
-            foreach (string m in mois)
+            Console.Write("");
+            int choix = Convert.ToInt32(Console.ReadLine());
+            if (choix == 1)
             {
-                if (m == "Décembre")
-                {
-
-                    Console.WriteLine($"| {m,-10} | {salaireMensuelNet * (1+primeNoel/100):F2} |");
-                }
-                
-                else if (m == "Août")
-                {
-                    Console.WriteLine($"| {m,-10} | {0:F2} |");
-                }
-
-                else
-                {
-                    // Salaire fixe pour tous les mois
-                    Console.WriteLine($"| {m,-10} | {salaireMensuelNet:F2} |");
-                }
+                Fonction.SalaireMensuelNet();
+            }
+            else if (choix == 2)
+            {
+                Fonction.TableauSalaire();
             }
 
-            Console.WriteLine("+------------+---------+");
+
         }
         catch (FormatException)
         {
